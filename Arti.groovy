828e5b3b6533e666ea9555b623a2e80d3546acf4
mission-control <mission-control@mission-control-1998279598-b6vs0>
2017-10-13 22:12:13 +0000

    File changed by admin

--- Arti.groovy
+++ Arti.groovy
@@ -1,2 +1,9 @@
-repo = userInput(name: "Repository to replicate:", type: "REPOSITORY")
-other = userInput(name: "Instances to replicate to:", type: "INSTANCE", multivalued: true)
+//repo = userInput(name: "Repository to replicate:", type: "REPOSITORY")
+//other = userInput(name: "Instances to replicate to:", type: "INSTANCE", multivalued: true)
+
+artifactory("Art-1") {
+localRepository("docker-local-a") {
+  packageType "docker"
+  description "My local Docker registry-vvvvvv"
+}
+}
