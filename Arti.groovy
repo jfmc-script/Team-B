85bc09a843f85dc388b8c49859d5078fbb120120
mission-control <mission-control@mission-control-1998279598-b6vs0>
2017-10-13 21:02:03 +0000

    File changed by admin

--- Arti.groovy
+++ Arti.groovy
@@ -1,1 +1,24 @@
-userInput(name: "foobar", type: "REPOSITORY")
+stringInput = userInput (
+    type : "STRING", // "BOOLEAN", "INTEGER", "INSTANCE", "REPOSITORY"
+    description : "String input"
+)
+
+booleanInput = userInput (
+    type : "BOOLEAN", //"INTEGER", "INSTANCE", "REPOSITORY"
+    description : "Boolean input"
+)
+
+integerInput = userInput (
+    type : "INTEGER", //"INSTANCE", "REPOSITORY"
+    description : "Integer input"
+)
+
+instanceInput = userInput (
+    type : "INSTANCE", // "REPOSITORY"
+    description : "Instance input"
+)
+
+repositoryInput = userInput (
+    type : "REPOSITORY",
+    description : "Repository input"
+)
