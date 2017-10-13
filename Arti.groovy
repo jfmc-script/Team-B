a5db1e419e16c99c03b7472216cff03b664f4ad2
mission-control <mission-control@mission-control-1998279598-b6vs0>
2017-10-13 23:17:52 +0000

    File changed by admin

--- Arti.groovy
+++ Arti.groovy
@@ -5,7 +5,7 @@ other = userInput(name: "Instances to replicate to:", type: "ARTIFACTORY", multi
 
 artifactory('Art-1') {
 	localRepository('generic-test') {
-  		description other.toString()
+  		description other.toString() + ' - ' + repo.toString()
   		packageType "generic"
 	}
 }
