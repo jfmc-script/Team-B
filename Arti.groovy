0682d8c28bc3abe34ca41ba94cab1e170e8508fa
mission-control <mission-control@mission-control-1998279598-b6vs0>
2017-10-13 21:07:49 +0000

    File changed by admin

--- Arti.groovy
+++ Arti.groovy
@@ -1,3 +1,2 @@
-main = userInput(name: "Instance to add", type: "INSTANCE")
-other = userInput(name: "Other instances", type: "INSTANCE", multivalued: true)
-repo = userInput(name: "Repository Name", type: "REPOSITORY")
+repo = userInput(name: "Repository to replicate:", type: "REPOSITORY")
+other = userInput(name: "Instances to replicate to:", type: "INSTANCE", multivalued: true)
