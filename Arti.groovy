7b78f4471e577b2d6eb974c8cb2aea221f09b290
mission-control <mission-control@mission-control-1998279598-b6vs0>
2017-10-13 22:27:58 +0000

    File changed by admin

--- Arti.groovy
+++ Arti.groovy
@@ -1,4 +1,4 @@
 // ask user for repository to replicate
 repo = userInput(name: "Repository to replicate:", type: "REPOSITORY")
 // ask user for instances to replicate to
-other = userInput(name: "Instances to replicate to:", type: "INSTANCE", multivalued: true)
+other = userInput(name: "Instances to replicate to:", type: "ARTIFACTORY", multivalued: true)
