e4610beaddcef7387e94e7efbe9d53fcb17193a5
mission-control <mission-control@mission-control-1998279598-b6vs0>
2017-10-13 21:00:19 +0000

    File changed by admin

--- Arti.groovy
+++ Arti.groovy
@@ -1,6 +1,1 @@
-artifactory("Art-1") {
-  localRepository("docker-local-b") {
-    packageType "docker"
-    description "My local Docker registry-vvvvvv"
-  }
-}
+userInput(name: "foobar", type: "INSTANCE", multivalued: true)
