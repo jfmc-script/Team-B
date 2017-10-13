2282c748e59845b8d77e3fc2d1829e7d47678043
mission-control <mission-control@mission-control-1998279598-b6vs0>
2017-10-13 21:04:17 +0000

    File changed by admin

--- Arti.groovy
+++ Arti.groovy
@@ -1,1 +1,2 @@
-UI = userInput(name: "foobar", type: "INSTANCE", multivalued: true)
+UI2 = userInput(name: "Instance to add", type: "INSTANCE")
+UI = userInput(name: "Other instances", type: "INSTANCE", multivalued: true)
