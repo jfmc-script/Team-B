6254a27c74228f0ea577b2a34fe44a735741f65b
mission-control <mission-control@mission-control-3690558510-2wwj5>
2017-11-02 14:12:42 +0000

    File changed by admin

--- arti.groovy
+++ arti.groovy
@@ -1,6 +1,6 @@
 artifactory('Denver'){
 localRepository("my-repository") {
-  description userInput (
+  packageType userInput (
     type : "STRING",
     value : "This is a generic description",
     description : "Please provide a description"
