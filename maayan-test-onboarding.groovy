9d1d5592a06dce5457c03efd3a4a382e059e8bb4
jfrogmc <jfrogmc@support-mc-tests-debian.c.jfrog-dev.internal>
2018-06-18 14:09:10 +0000

    File changed by admin

---
+++ maayan-test-onboarding.groovy
@@ -0,0 +1,111 @@
+repokey = userInput (
+  type : "STRING",
+  name : "RepoKey",
+  description : "Please provide a Repository Key"
+)
+
+maturity = userInput (
+  type : "STRING",
+  name : "Maturity",
+  description : "Please provide a value for maturity (example - dev, stage or prod)"
+)
+
+packagetype = userInput (
+  name : "Package Type",
+  type : "PACKAGE_TYPE",
+  value : "",
+  description : "Please provide a value for package type"
+)
+
+location = userInput (
+  type : "STRING",
+  name : "Location",
+  description : "Please provide a value for location (example - local, remote)"
+)
+
+art = userInput (
+  type : "ARTIFACTORY",
+  name : "Instance",
+  description : "Please select the artifactory instance to run against"
+)
+
+userName = userInput (
+  type : "STRING",
+  name : "User Name",
+  description : "Please provide a username"
+)
+
+groupNames = userInput (
+  type : "STRING",
+  name : "Group Names",
+  description : "Please provide a group/s name (comma separated, if multiple groups need to be created)"
+)
+
+permissionName = userInput (
+  type : "STRING",
+  name : "Permission Name",
+  description : "Please provide a permission name"
+)
+
+permissions = userInput (
+  type : "STRING",
+  name : "Permissions",
+  description : "Please enter the permissions that need to be granted (manage,delete,deploy,annotate,read) comma separated, if multiple permissions need to be granted"
+)
+
+repoNames = userInput (
+  type : "STRING",
+  name : "Repository Names",
+  description : "Please provide a repo/s name (comma separated, if multiple repositories need to be granted permissions)"
+)
+//construct a namespace
+//team-technology-maturity-location
+def localName = repokey + "-" + packagetype + "-" + maturity + "-" + location
+
+artifactory(art.name) {
+  localRepository(localName) {
+    description "Public description"
+    notes "Some internal notes"
+    archiveBrowsingEnabled false
+    packageType packagetype
+  }
+  security {
+    groups {
+      for(groupName in groupNames.split(",")) {
+        group(groupName) {
+          description ''
+          autoJoin false
+        }
+      }
+    }
+    users {
+      user(userName) {
+        email 'login_1@jfrog.com'
+        password 'password'
+        admin false
+        profileUpdatable false
+        internalPasswordDisabled false
+        groups (groupNames.split(",") as List)
+      }
+    }
+    permissions {
+      permission(permissionName) {
+        includesPattern '**'
+        excludesPattern ''
+        anyLocal false
+        anyRemote false
+        anyDistribution false
+        repositories (repoNames.split(",") as List)
+        users {
+          "$userName" (permissions.split(",") as List)
+        }
+        groups {
+          def myGroups = groupNames.split(",") as List
+          myGroups.each {
+            "$it" (permissions.split(",") as List)
+          }
+        }
+      }
+    }
+  }
+}
